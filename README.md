eebf9f39027ae0dbe054f0d7a0f1f382f887c3d8
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-26 22:51:27 +0100

    Fixed bug with deleting restaurants

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Yelp Clone
 
-A Yelp clone built using Ruby on Rails using TDD.
+A Yelp clone built on Ruby on Rails using TDD.
 
 ## Stack
 * Ruby on Rails
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
-  has_many :endorsements
+  has_many :endorsements, dependent: :destroy
 
   validates :rating, inclusion: (1..5)
   validates :user,
