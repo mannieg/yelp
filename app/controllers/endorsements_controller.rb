0170fc7d72e13da7cc669b2f0e3ad66f05e4e459
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-27 10:47:17 +0100

    Formatting cleanup

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,9 +1,10 @@
 class EndorsementsController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
-  
+
   def create
     @review = Review.find(params[:review_id])
     @review.endorsements.create
     render json: {new_endorsement_count: @review.endorsements.count}
- end
+  end
+  
 end
